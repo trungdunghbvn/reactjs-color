d351a41d539d8d55cc71d4165fa23bebad01a662
Trung Dung <trungdunghbvn@gmail.com>
2018-08-20 21:25:48 +0700

    v1.0.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/Hello';
+
+ReactDOM.render(
+    <Hello />,
+  document.getElementById('root')
+);
