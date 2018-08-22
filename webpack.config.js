a5ae3d02b68697497e2715035f9de88dcb1c35f5
Trung Dung <trungdunghbvn@gmail.com>
2018-08-22 22:30:07 +0700

    v1.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,14 @@ var config = {
         loader : 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV':JSON.stringify('production')
+      }
+    })
+  ]
 };
 
 module.exports = config;
